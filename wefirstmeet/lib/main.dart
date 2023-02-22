ed2d87e1f215bc53f8bc587bc51f8c309eae5abb
황정현 <jhhwang@wemakeprice.com>
2023-02-23 00:53:32 +0900

    미니프로젝트 - wemeety

---
+++ wefirstmeet/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'home_screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        textTheme: TextTheme(
+          displayLarge: TextStyle(
+              color: Colors.white, fontFamily: 'parisienne', fontSize: 80.0
+          ),
+          displayMedium:
+        )
+      ),
+      home: HomeScreen(),
+    ),
+  );
+}
+
