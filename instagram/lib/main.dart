c6d20ac172e2ea980e3255fabb44a3062da6f482
황정현 <jhhwang@wemakeprice.com>
2023-04-04 23:40:02 +0900

    feat instagram 클론코딩 - http 통신

--- instagram/lib/main.dart
+++ instagram/lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:instagram/style.dart' as style;
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter/rendering.dart';
 
 
 void main() {
@@ -20,37 +23,73 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-class HomeBody extends StatelessWidget {
-  const HomeBody({Key? key}) : super(key: key);
+class HomeBody extends StatefulWidget {
+  const HomeBody({Key? key, this.data}) : super(key: key);
+  final data;
+
+  @override
+  State<HomeBody> createState() => _HomeBodyState();
+}
+
+class _HomeBodyState extends State<HomeBody> {
+
+  var scroll = ScrollController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    scroll.addListener(() {
+      if (scroll.position.pixels == scroll.position.maxScrollExtent) {
+        print('끝임');
+
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Expanded(
-          child: Image(
-            image: AssetImage('assets/images/IMG_0205.PNG'),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(11.0),
-          child: Column(
-            children: [
-              Text('좋아요 100'),
-              Text('글쓴이'),
-              Text('글내용!!'),
-            ],
-          ),
-        )
-      ],
-    );
+    if (widget.data.isNotEmpty) {
+      return ListView.builder(itemCount: widget.data.length, controller: scroll,itemBuilder: (c, i) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image.network(widget.data[i]['image'],),
+            Text('좋아요 ${widget.data[i]['likes'].toString()}'),
+            Text(widget.data[i]['user']),
+            Text(widget.data[i]['content'])
+          ],
+        );
+      });
+    }
+
+    return CircularProgressIndicator();
   }
 }
 
 
 class _MyAppState extends State<MyApp> {
   var tab = 0;
+  var data = [];
+
+  getData() async {
+    var result = await http.get(Uri.parse('https://codingapple1.github.io/app/data.json'));
+
+    if (result.statusCode != 200) {
+      throw Exception('초기 데이터를 불러오지 못했습니다.');
+    }
+
+    setState(() {
+      data = jsonDecode(result.body);
+      print(data);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -67,7 +106,7 @@ class _MyAppState extends State<MyApp> {
           ],
         ),
       body: [
-        HomeBody(),
+        HomeBody(data : data),
         Text('shop')
       ][tab],
       bottomNavigationBar: BottomNavigationBar(
